69a459c5c79dfde22fa5dba4b100dfb7bd0be327
Idrissa <idiberthe19@gmail.com>
2023-10-22 23:55:35 -0400

    CART FUNCTIONALITIES

--- components/ui/navbar/navbar-actions.tsx
+++ components/ui/navbar/navbar-actions.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-
+import { ShoppingBag } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-
+import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -14,7 +15,7 @@ const NavbarActions = () => {
     }, []);
 
     const router = useRouter();
-
+    const cart = useCart();
 
     if (!isMounted) {
         return null;
@@ -22,7 +23,15 @@ const NavbarActions = () => {
 
     return (
         <div className="ml-auto flex items-center gap-x-4">
-            Navbar Actions
+            <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+                <ShoppingBag
+                    size={20}
+                    color="white"
+                />
+                <span className="ml-2 text-sm font-medium text-white">
+                    {cart.items.length}
+                </span>
+            </Button>
         </div>
     );
 }
