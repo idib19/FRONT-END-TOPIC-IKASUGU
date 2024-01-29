76cd9696bdd22df5ccf14a35341a74d49d80b666
Idrissa <idiberthe19@gmail.com>
2024-01-28 23:57:43 -0500

    PUSHING STABLE VERSION 1

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {}
 
 module.exports = {
-    async rewrites() {
-        return [
-          {
-            source: '/:store*',
-            destination: '/', // The :path parameter isn't used here so will be automatically passed in the query
-          },
-        ]
-      },
-
     images: {
         remotePatterns: [
             {
